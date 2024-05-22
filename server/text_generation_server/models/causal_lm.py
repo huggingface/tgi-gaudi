861d4cea6dbd123c3274410019751688973aa5d2
Jimin Ha <jha@habana.ai>
2024-05-22 11:04:53 -0700

    Only enable the flash attention flag fort the llama and mistral

--- server/text_generation_server/models/causal_lm.py
+++ server/text_generation_server/models/causal_lm.py
@@ -666,12 +666,12 @@ class CausalLM(Model):
             kwargs["attn_softmax_bf16"] = True
             kwargs["trim_logits"] = True
 
-        if os.getenv("USE_FLASH_ATTENTION", "false").lower() == "true":
-            kwargs["use_flash_attention"] = True
-        if os.getenv("FLASH_ATTENTION_RECOMPUTE", "false").lower() == "true":
-            kwargs["flash_attention_recompute"] = True
-        if os.getenv("FLASH_ATTENTION_CAUSAL_MASK", "false").lower() == "true":
-            kwargs["flash_attention_causal_mask"] = True
+            if os.getenv("USE_FLASH_ATTENTION", "false").lower() == "true":
+                kwargs["use_flash_attention"] = True
+            if os.getenv("FLASH_ATTENTION_RECOMPUTE", "false").lower() == "true":
+                kwargs["flash_attention_recompute"] = True
+            if os.getenv("FLASH_ATTENTION_CAUSAL_MASK", "false").lower() == "true":
+                kwargs["flash_attention_causal_mask"] = True
 
         self.speculate = get_speculate()
 
