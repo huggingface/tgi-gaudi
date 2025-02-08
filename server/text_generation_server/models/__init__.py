213896f004583881da34dd67147cd28f54e1eb7d
yuanwu <yuan.wu@intel.com>
2025-02-08 03:12:00 +0000

    Enable the mllama
    Signed-off-by: yuanwu <yuan.wu@intel.com>

--- server/text_generation_server/models/__init__.py
+++ server/text_generation_server/models/__init__.py
@@ -17,20 +17,21 @@ from text_generation_server.models.causal_lm import CausalLM
 from text_generation_server.models.bloom import BLOOM
 from text_generation_server.models.starcoder import StarCoder
 from text_generation_server.models.vlm_causal_lm import VlmCausalLM
-#from text_generation_server.models.mllama_causal_lm import MllamaCausalLM
+from text_generation_server.models.mllama_causal_lm import MllamaCausalLM
 from text_generation_server.models.custom_modeling.llava_next import (
     LlavaNextForConditionalGeneration,
 )
-# from text_generation_server.models.mllama_causal_lm import MllamaCausalLMBatch
-# from text_generation_server.models.custom_modeling.mllama import (
-#     MllamaForConditionalGeneration,
-# )
+from text_generation_server.models.mllama_causal_lm import MllamaCausalLMBatch
+from text_generation_server.models.custom_modeling.mllama import (
+     MllamaForConditionalGeneration,
+)
 from text_generation_server.utils.adapter import (
     AdapterParameters,
     build_layer_weight_lookup,
     load_and_merge_adapters,
     AdapterInfo,
 )
+from text_generation_server.adapters.lora import LoraWeights
 
 
 from optimum.habana.transformers.modeling_utils import adapt_transformers_to_gaudi
@@ -38,6 +39,19 @@ from optimum.habana.transformers.modeling_utils import adapt_transformers_to_gau
 
 # Disable gradients
 torch.set_grad_enabled(False)
+
+class ModelType(enum.Enum):
+     MLLAMA = {
+        "type": "mllama",
+        "name": "Mllama",
+        "url": "https://huggingface.co/meta-llama/Llama-3.2-11B-Vision-Instruct",
+        "multimodal": True,
+    }
+
+__GLOBALS = locals()
+for data in ModelType:
+    __GLOBALS[data.name] = data.value["type"]
+
 
 
 def get_model(
@@ -194,6 +208,20 @@ def get_model(
             speculator=speculator,
             dtype=dtype,
             trust_remote_code=trust_remote_code,
+        )
+    
+    if model_type == MLLAMA:
+        return MllamaCausalLM(
+            model_id=model_id,
+            model_class=MllamaForConditionalGeneration,
+            batch_class=MllamaCausalLMBatch,
+            revision=revision,
+            quantize=quantize,
+            speculator=speculator,
+            dtype=dtype,
+            default_dtype=torch.bfloat16,
+            trust_remote_code=trust_remote_code,
+            lora_adapter_ids=lora_adapter_ids,
         )
 
     if model_type in modeling_auto.MODEL_FOR_CAUSAL_LM_MAPPING_NAMES:
